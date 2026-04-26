34c6010d378528c8709eec4b95f786c1adc61f5e
fauziramadhani <93056712+fauziramadha@users.noreply.github.com>
2026-04-26 16:55:38 +0700

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,17 +1,26 @@
 // File: api/auth.js (Vercel Serverless Function)
-const ImageKit = require("imagekit");
+// Menggunakan library
+const ImageKit = require("@imagekit/nodejs"); 
 
 const imagekit = new ImageKit({
   urlEndpoint: "https://ik.imagekit.io/tumbal",
-  publicKey: "public_XKAN1YH/RnUD5N+4hS/qIvqDJNc=",
-  // Private Key disembunyikan di Environment Variables Vercel demi keamanan
+  // Memanggil Public Key dari Environment Variables yang sudah di pasang
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY, 
+  // Private Key aman
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY 
 });
 
 module.exports = function (req, res) {
-  // Mengizinkan CORS (Cross-Origin Resource Sharing)
+  // Mengizinkan CORS agar bisa diakses dari domain web kamu
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+
+  // Menangani preflight request untuk browser
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   
   try {
     const result = imagekit.getAuthenticationParameters();
